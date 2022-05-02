fecf31d74ce3fd18756578f5dc68dde59c150880
NickAzp <69650461+NickAzp@users.noreply.github.com>
2022-05-01 17:33:23 -0700

    Added failed test file

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,2 +1,5 @@
 public class SkillDemo {
+    public int multiply(int a, int b) {
+        return a*b;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(4, SkillDemo.multiply(2,3));
+    }
+}
